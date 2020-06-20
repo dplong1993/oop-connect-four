08acd415cba09701e13a7b9efb70039df00c81fd
Dillon Long <dplong@crimson.ua.edu>
2020-06-20 15:12:27 -0700

    Refactored code in row-win-inspector.js to make the code easier to read.

--- row-win-inspector.js
+++ row-win-inspector.js
@@ -9,11 +9,10 @@ export default class RowWinInspector{
         const token2 = this.columns[1].getTokenAt(i);
         const token3 = this.columns[2].getTokenAt(i);
         const token4 = this.columns[3].getTokenAt(i);
-        // console.log(token1, token2, token3, token4);
-        if (token1 !== null && token1 === token2 && token1 === token3 && token1 === token4) {
-          //console.log("Row win!");
-          return token1;
-        }
+        if (token1 !== null &&
+            token1 === token2 &&
+            token1 === token3 &&
+            token1 === token4) return token1;
       }
       return 0;
   }
