50d04e9bd4597cd2590a1896c2aff3ba95f8834f
Dillon Long <dplong@crimson.ua.edu>
2020-06-20 15:02:48 -0700

    Refactored code in column-win-inspector.js to make the code easier to read.

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -1,20 +1,15 @@
 export default class ColumnWinInspector {
     constructor(column) {
-        //console.log("Column passed into CWI is", column);
         this.column = column;
     }
 
     inspect () {
         for (let i = 5; i > 2; i--) {
             const playernumber = this.column.getTokenAt(i);
-            //console.log("Current column is ", this.column, 'i is', i, "PlayerNum is", playernumber);
-            if (playernumber !== 1 && playernumber !== 2) {
-                return 0;
-            } else if (playernumber === this.column.getTokenAt(i - 1)) {
+            if (playernumber !== 1 && playernumber !== 2) return 0;
+            else if (playernumber === this.column.getTokenAt(i - 1)) {
                 if (playernumber === this.column.getTokenAt(i - 2)) {
-                    if (playernumber === this.column.getTokenAt(i-3)) {
-                        return playernumber;
-                    }
+                    if (playernumber === this.column.getTokenAt(i-3)) return playernumber;
                 }
             }
         }
